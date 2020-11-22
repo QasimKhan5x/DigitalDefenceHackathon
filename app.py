f007b0f2ed60a44f8ec0bdef60a127986fa80bec
FahadZaheerfzr <fahadzaheer19@gmail.com>
2020-11-23 03:09:16 +0500

    Another Model

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 from os import abort
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
-
 
 @app.route('/')
 def hello_world():
     return render_template('dashboard-1.html')
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
 
 
 @app.route('/info')
